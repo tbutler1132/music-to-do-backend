f8b12f61934c0f5e7d9ac09e705c01ce90af4fe0
Timothy Butler <tbutler1132@gmail.com>
2021-06-22 12:20:50 -0400

    changed port

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use('/login', loginRoutes)
 
 const CONNECTION_URL = 'mongodb+srv://tbutler1132:1234@cluster0.jckqb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
-const PORT = process.env.PORT|| 7000;
+const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
