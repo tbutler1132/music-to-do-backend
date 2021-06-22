2eeb7a921d74e838522bf93e018e34d21ec175e7
Timothy Butler <tbutler1132@gmail.com>
2021-06-22 12:56:08 -0400

    cors again

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const require = createRequire(import.meta.url);
 
 import userRoutes from './routes/users.js'
 import loginRoutes from './routes/login.js'
-import {authenticateToken} from './routes/auth.js'
 
 require('dotenv').config()
 
@@ -15,29 +14,12 @@ const app = express();
 
 app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested, Content-Type, Accept Authorization"
-  )
-  if (req.method === "OPTIONS") {
-    res.header(
-      "Access-Control-Allow-Methods",
-      "POST, PUT, PATCH, GET, DELETE"
-    )
-    return res.status(200).json({})
-  }
-  next()
-})
-
-// app.use(cors());
+app.use(cors());
 
 app.use('/users', userRoutes)
 app.use('/login', loginRoutes)
 
-const CONNECTION_URL = 'mongodb+srv://tbutler1132:1234@cluster0.jckqb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const CONNECTION_URL = 'mongodb://tbutler1132:1234@tbutler1132-to-do-mongo-production/admin'
 
 const PORT = process.env.PORT|| 5000;
 
