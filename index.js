e298e76c319926a9f3aad6124645c2a98557e3bc
Timothy Butler <tbutler1132@gmail.com>
2021-06-23 14:37:03 -0400

    removed morgan

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import {createRequire} from 'module'
-import morgan from 'morgan'
 const require = createRequire(import.meta.url);
 
 import userRoutes from './routes/users.js'
@@ -10,17 +9,13 @@ import loginRoutes from './routes/login.js'
 import {authenticateToken} from './routes/auth.js'
 
 require('dotenv').config()
-const { createProxyMiddleware } = require('http-proxy-middleware');
+
 
 const app = express();
-
-app.use(morgan('dev'))
 
 app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
-
-//PROXY ENDPOINTS
 
 app.use('/users', userRoutes)
 app.use('/login', loginRoutes)
