1b90476581009c6a153faea52632363c3cbabfe8
Timothy Butler <tbutler1132@gmail.com>
2021-06-23 11:48:39 -0400

    proxy

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import express from 'express'
-import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import {createRequire} from 'module'
@@ -7,26 +6,39 @@ const require = createRequire(import.meta.url);
 
 import userRoutes from './routes/users.js'
 import loginRoutes from './routes/login.js'
+import {authenticateToken} from './routes/auth.js'
 
 require('dotenv').config()
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(express.json({ limit: '30mb', extended: true }))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
+
+//PROXY ENDPOINTS
+app.use('/proxy', createProxyMiddleware({
+  target: "https://tbutler1132-music-to-do-backend.zeet.app/",
+  changeOrigin: true,
+  pathRewrite: {
+      [`^/proxy`]: '',
+  },
+}));
 
 app.use('/users', userRoutes)
 app.use('/login', loginRoutes)
 
-const CONNECTION_URL = 'mongodb://tbutler:1132@tbutler1132-mongo-todo-production/admin'
+const CONNECTION_URL = 'mongodb+srv://tbutler1132:1234@cluster0.jckqb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
-const PORT = process.env.PORT|| 5000;
+const PORT = process.env.PORT|| 7000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
+app.get('/', (req, res) => {
+    res.send('I a running')
+})
 
 mongoose.set('useFindAndModify', false);
