5fb5a608b6bd8eaf7c0b6a9de1651c578a4cf8cd
Timothy Butler <tbutler1132@gmail.com>
2021-06-23 09:53:17 -0400

    body-parser

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import express from 'express'
+import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import {createRequire} from 'module'
-import { createProxyMiddleware  } from 'http-proxy-middleware'
 const require = createRequire(import.meta.url);
 
 import userRoutes from './routes/users.js'
@@ -10,29 +10,12 @@ import loginRoutes from './routes/login.js'
 
 require('dotenv').config()
 
-const options = {
-  target: 'https://tbutler1132-music-to-do-frontend.zeet.app', // target host
-  changeOrigin: true, // needed for virtual hosted sites
-  ws: true, // proxy websockets
-  pathRewrite: {
-    '^/proxy/': '/'
- },
-  router: {
-    // when request.headers.host == 'dev.localhost:3000',
-    // override target 'http://www.example.org' to 'http://localhost:8000'
-    'https://tbutler1132-music-to-do-backend.zeet.app/': 'http://localhost:5000',
-  },
-};
-
-const exampleProxy = createProxyMiddleware(options)
-
 const app = express();
 
 
-app.use(express.json({ limit: '30mb', extended: true }))
-app.use(express.urlencoded({ limit: '30mb', extended: true }))
-app.use('/proxy', exampleProxy)
-
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
 
 app.use('/users', userRoutes)
 app.use('/login', loginRoutes)
