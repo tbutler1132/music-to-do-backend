f293890c418e6cf98038a5b22f8eedf61b9a0869
Timothy Butler <tbutler1132@gmail.com>
2021-06-23 08:51:24 -0400

    FINAL

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use('/users', userRoutes)
 app.use('/login', loginRoutes)
 
-const CONNECTION_URL = 'mongodb+srv://tbutler1132:1234@cluster0.jckqb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const CONNECTION_URL = 'mongodb://tbutler:1132@tbutler1132-mongo-todo-production/admin'
 
 const PORT = process.env.PORT|| 5000;
 
